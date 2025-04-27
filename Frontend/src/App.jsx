bff3fd13c7466fb27c49db4bbaa124b4decd6dd3
itsabhay1 <abhayag41@gmail.com>
2025-04-28 02:02:37 +0530

    banner and footer added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import Footer from './components/footer';
 
 function App() {
   return(
     <>
     <Navbar/>
+    <Banner/>
+    <Footer/>
     </>
   );
 }
