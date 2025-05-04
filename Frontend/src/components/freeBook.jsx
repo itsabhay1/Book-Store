bbbb36996e24a65b3f089da4b08acbdc7c940c6b
itsabhay1 <abhayag41@gmail.com>
2025-05-04 16:52:30 +0530

    make slider automatic

--- Frontend/src/components/freeBook.jsx
+++ Frontend/src/components/freeBook.jsx
@@ -9,11 +9,13 @@ function FreeBook() {
   console.log(filterData)
   var settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
     initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 3000,
     responsive: [
       {
         breakpoint: 1024,
