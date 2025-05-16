e1e5cb8cae5325b3f3943175d8892d974cad28f5
itsabhay1 <abhayag41@gmail.com>
2025-05-16 22:54:57 +0530

    deployment error resolved

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,7 +15,10 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
 
 connetDB()
 .then(() => {
